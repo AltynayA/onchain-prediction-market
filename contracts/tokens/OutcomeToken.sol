5f350a1c332686fdef8efada2d7127df704444fb
altynay <goldmoon321123@gmail.com>
2026-04-29 18:53:28 +0500

    chore: apply forge fmt formatting

--- contracts/tokens/OutcomeToken.sol
+++ contracts/tokens/OutcomeToken.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.24;
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-
 contract OutcomeToken is ERC1155, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
@@ -19,28 +18,17 @@ contract OutcomeToken is ERC1155, AccessControl {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function mint(address to, uint256 id, uint256 amount)
-        external
-        onlyRole(MINTER_ROLE)
-    {
+    function mint(address to, uint256 id, uint256 amount) external onlyRole(MINTER_ROLE) {
         require(id == YES || id == NO, "Invalid outcome");
         _mint(to, id, amount, "");
     }
 
-    function burn(address from, uint256 id, uint256 amount)
-        external
-        onlyRole(MINTER_ROLE)
-    {
+    function burn(address from, uint256 id, uint256 amount) external onlyRole(MINTER_ROLE) {
         require(id == YES || id == NO, "Invalid outcome");
         _burn(from, id, amount);
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC1155, AccessControl)
-        returns (bool)
-    {
+    function supportsInterface(bytes4 interfaceId) public view override(ERC1155, AccessControl) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 }
