e70ff4cef47031651782f5cd2d24f3236221959a
altynay <goldmoon321123@gmail.com>
2026-04-29 18:18:03 +0500

    feat(token): implement GovernanceToken with ERC20Votes and Permit

---
+++ contracts/tokens/GovernanceToken.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
+
+contract GovernanceToken is ERC20Votes, ERC20Permit {
+    constructor(address initialHolder) ERC20("PredictToken", "PRED") ERC20Permit("PredictToken") {
+        _mint(initialHolder, 1_000_000 ether);
+    }
+
+    function _update(address from, address to, uint256 amount) internal override(ERC20, ERC20Votes) {
+        super._update(from, to, amount);
+    }
+
+    function nonces(address owner) public view override(ERC20Permit, Nonces) returns (uint256) {
+        return super.nonces(owner);
+    }
+}
