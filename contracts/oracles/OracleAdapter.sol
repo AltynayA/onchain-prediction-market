5800055a51049d60e5829dc860cdb3baf1722c06
altynay <goldmoon321123@gmail.com>
2026-05-18 03:52:20 +0500

    fix: remove unused imports, fix pragma in PredictionToken

--- contracts/oracles/OracleAdapter.sol
+++ contracts/oracles/OracleAdapter.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {IOracleAdapter} from "../core/PredictionMarket.sol";
-
 interface AggregatorV3Interface {
     function latestRoundData() external view returns (uint80, int256 answer, uint256, uint256 updatedAt, uint80);
 }
--- contracts/tokens/OutcomeToken.sol
+++ contracts/tokens/OutcomeToken.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {IOutcomeToken} from "../core/PredictionMarket.sol";
-
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
